6af34f978dace4c7821e9cd7b797d268fbb2a3ac
Lucas Talamini <lucastalamini@hotmail.com>
2020-08-02 17:01:10 -0300

    English final

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ getQuotes().then((quotes) => {
   }
   let message = "";
   message += `"${quotes.quote}" <br><br><i>- ${quotes.author}</i>`;
-  console.log(quotes.author, quotes.quote);
+  // console.log(quotes.author, quotes.quote);
 
   document.querySelector(".message").innerHTML = message;
 });
